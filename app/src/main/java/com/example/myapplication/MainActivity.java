15ae8cfe1f2c489626850747efcda30cac39e17b
SWAT10101 <swat1994911@gmail.com>
2019-10-25 06:17:58 +0300

    change login register layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = findViewById(R.id.vf);
+
+    }
+
+
+
+    public void viewRegister(View view) {
+        viewFlipper.setDisplayedChild(1);
+    }
+
+    public void viewLogin(View view) {
+        viewFlipper.setDisplayedChild(2);
     }
 }
